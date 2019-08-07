c15cae50d65f1a70e8d205ebc644ff6041da93fb
liuguowei <424005496@qq.com>
2019-08-07 15:04:41 +0800

    门面框架

---
+++ project_mgr/test_mgr/src/main/java/com/test/TestWebMgrApplication.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 门户启动
+ * @Author: guowei liu
+ * @Date: 2019/8/7
+ */
+@SpringBootApplication
+public class TestWebMgrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebMgrApplication.class, args);
+    }
+}
---
+++ project_mgr/test_mgr/src/main/java/com/test/controller/test/TestController.java
@@ -0,0 +1,21 @@
+package com.test.controller.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: guowei liu
+ * @Date: 2019/8/7
+ */
+@RestController
+@RequestMapping("/web/mgr/test")
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "百度！";
+    }
+}
