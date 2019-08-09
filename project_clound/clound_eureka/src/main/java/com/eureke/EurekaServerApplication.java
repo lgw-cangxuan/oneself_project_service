5005004c1c1c6eb649fe7f02a8fdddc440badc91
liuguowei <424005496@qq.com>
2019-08-09 17:04:24 +0800

    eureka

--- project_clound/clound_eureka/src/main/java/com/eureke/EurekaServerApplication.java
+++ project_clound/clound_eureka/src/main/java/com/eureke/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.eureke;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
