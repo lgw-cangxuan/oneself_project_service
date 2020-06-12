15312b577b6256a24e3142bf8db82bb7752b3645
liuguowei <424005496@qq.com>
2020-06-12 10:20:19 +0800

    降低springBoot，springCloud版本

--- project_mgr/project_service/src/main/java/com/service/ProjectServiceApplication.java
+++ project_mgr/project_service/src/main/java/com/service/ProjectServiceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 服务提供者
+ * @author liuguowei
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProjectServiceApplication {
--- project_mgr/project_web/src/main/java/com/web/ProjectWebApplication.java
+++ project_mgr/project_web/src/main/java/com/web/ProjectWebApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 服务消费者
+ * @author liuguowei
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class ProjectWebApplication {
