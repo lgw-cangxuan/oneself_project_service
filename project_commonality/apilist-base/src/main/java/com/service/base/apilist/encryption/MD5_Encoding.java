b05deb7f4a417f90e5681ae680c99e685333a333
liuguowei <424005496@qq.com>
2020-08-18 16:58:57 +0800

    格式化代码

--- project_commonality/apilist-base/src/main/java/com/service/base/apilist/encryption/MD5_Encoding.java
+++ project_commonality/apilist-base/src/main/java/com/service/base/apilist/encryption/MD5_Encoding.java
@@ -15,57 +15,57 @@ public class MD5_Encoding {
     private final static StringBuilder SB = new StringBuilder("01234md5encoding");
 
     /**
-       * 对字符串md5加密(小写+数字)
-       *
-       * @param str 传入要加密的字符串
-       * @return  MD5加密后的字符串
-       */
-     public static String lowerMD5(String str) {
-         try {
-                 // 生成一个MD5加密计算摘要
-                 MessageDigest md = MessageDigest.getInstance("MD5");
-                 // 计算md5函数
-                 md.update(str.getBytes());
-                 // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-                 // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-                 return new BigInteger(1, md.digest()).toString(16);
-             } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-             }
-     }
+     * 对字符串md5加密(小写+数字)
+     *
+     * @param str 传入要加密的字符串
+     * @return MD5加密后的字符串
+     */
+    public static String lowerMD5(String str) {
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            return new BigInteger(1, md.digest()).toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     /**
-    * 对字符串md5加密(大写+数字)
-    *
-    * @param s 传入要加密的字符串
-    * @return  MD5加密后的字符串
-    */
-     public static String upperMD5(String s) {
-         char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         try {
-                 byte[] btInput = s.getBytes();
-                 // 获得MD5摘要算法的 MessageDigest 对象
-                 MessageDigest mdInst = MessageDigest.getInstance("MD5");
-                 // 使用指定的字节更新摘要
-                 mdInst.update(btInput);
-                 // 获得密文
-                 byte[] md = mdInst.digest();
-                 // 把密文转换成十六进制的字符串形式
-                 int j = md.length;
-                 char str[] = new char[j * 2];
-                 int k = 0;
-                 for (int i = 0; i < j; i++) {
-                         byte byte0 = md[i];
-                         str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                         str[k++] = hexDigits[byte0 & 0xf];
-                     }
-                 return new String(str);
-             } catch (Exception e) {
-                 e.printStackTrace();
-                 return null;
-             }
-     }
+     * 对字符串md5加密(大写+数字)
+     *
+     * @param s 传入要加密的字符串
+     * @return MD5加密后的字符串
+     */
+    public static String upperMD5(String s) {
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        try {
+            byte[] btInput = s.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     /**
      * 生成含固定盐的密码
@@ -133,12 +133,12 @@ public class MD5_Encoding {
     public static void main(String[] args) {
         // 加密+加盐
         String password1 = generateRandomSalt("admin");
-        System.out.println("结果：" + password1 + "   长度："+ password1.length());
+        System.out.println("结果：" + password1 + "   长度：" + password1.length());
         // 解码
         System.out.println(verify("admin", password1));
         // 加密+加盐
-        String password2= generateRandomSalt("admin");
-        System.out.println("结果：" + password2 + "   长度："+ password2.length());
+        String password2 = generateRandomSalt("admin");
+        System.out.println("结果：" + password2 + "   长度：" + password2.length());
         // 解码
         System.out.println(verify("admin", password2));
     }
